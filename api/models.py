ec721492a13c8a8b48fc8b5c34d28b9444ed5b2a
rsato1007 <robertmsato@gmail.com>
2022-06-12 20:46:55 -0600

    adding member model to django

--- api/models.py
+++ api/models.py
@@ -6,3 +6,39 @@ from django.contrib.auth.models import AbstractUser
 # Documentation can be found here: https://docs.djangoproject.com/en/4.0/topics/auth/customizing/#substituting-a-custom-user-model
 class User(AbstractUser):
     pass
+
+class Member(models.Model):
+    # Documentation on choice field options can be found here: https://docs.djangoproject.com/en/4.0/ref/models/fields/#django.db.models.Field.choices
+    SUFFIX_CHOICEs = [
+        ('Jr', 'Junior'),
+        ('Sr', 'Senior'),
+        ('II', 'Second'),
+        ('III', 'Third'),
+        ('IV', 'Fourth'),
+        ('Mr', 'Mister'),
+        ('Mrs', 'Mistress'),
+        ('Ms', 'Missus'),
+    ]
+    PRONOUN_CHOICES = [
+        ('He', 'He/Him/His'),
+        ('She', 'She/Her/Hers'),
+        ('They', 'They/Them/Theirs'),
+    ]
+    first_name = models.CharField(max_length = 100)
+    last_name = models.CharField(max_length = 100)
+    middle_initial = models.CharField(max_length = 1, null = True)
+    suffix = models.CharField(max_length = 10, null = True, choices=SUFFIX_CHOICEs)
+    pronouns = models.CharField(max_length = 25, null = True, choices=PRONOUN_CHOICES)
+    verbal_password = models.CharField(max_length=100)
+    ssn = models.IntergerField(validators=[MinValueValidator(100000000), MaxValueValidator(999999999)])
+    date_of_birth = models.DateField()
+    date_of_death = models.DateField(null = True)
+    join_date = models.DateField(auto_now_add=True)
+    address = models.CharField(max_length=250)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=25)
+    zip_code = models.CharField(max_length=10)
+    email = models.EmailField(null = True)
+    home_number = models.IntergerField(null = True)
+    mobile_number = models.IntergerField(null = True)
+    work_number = models.IntergerField(null = True)
--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,13 @@
 import React from 'react';
 
+// Adding this as a reminder to not forget to add
+// validators to client side when doing things like
+// adding memebr number, new member, etc.
+
+// Another thing, I found a possible API for address.
+// Use this when doing address: https://www.smarty.com/articles/usps-api
+// This might be the same one that prodigy uses.
+
 export default function App() {
     return (
         <div>Hello World</div>
